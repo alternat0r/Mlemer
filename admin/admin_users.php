834f603aa6118effd4e981ab1277c0e7b17effb9
alternat0r <alternator99@gmail.com>
2016-05-11 23:53:02 +0800

    Fixed user management table layout.

--- admin/admin_users.php
+++ admin/admin_users.php
@@ -4,10 +4,10 @@
 	<thead>
 		<tr>
 			<th>#</th>
+			<th style="text-align: center">Icon</th>
 			<th>Timestamp</th>
 			<th>User Name</th>
 			<th>IP/Hostname</th>
-			<th>UID</th>
 			<th style="text-align: center"><div class="glyphicon glyphicon-cog"></div></th>
 		</tr>
 	</thead>
@@ -21,6 +21,7 @@
 							if ( $row['user_realname'] != "Administrator" and $row['user_realname'] != "" ) {
 								echo "<tr>\n";
 								echo "	<td style=\"vertical-align: middle;\">" . $user_count . "</td>\n";
+								echo "	<td style=\"text-align: center; vertical-align: middle;\"><div style=\"align: center\" title=\"".$row['user_uid']."\"><i class=\"glyphicon glyphicon-user\"></i></div></td>\n";
 								echo "	<td style=\"vertical-align: middle;\">" . $row['timestamp'] . "</td>\n";
 								echo "	<td style=\"vertical-align: middle;\">";
 								echo $row['user_loginname']."<br/>";
@@ -30,8 +31,7 @@
 								echo $row['user_ip']."<br/>";
 								echo "		<i>".$row['user_hostname']."</i>";
 								echo "	</td>\n";
-								echo "	<td style=\"vertical-align: middle;\">" . $row['user_uid'] . "</td>\n";
-								echo "	<td style=\"vertical-align: middle;\"><a href=\"#\" class=\"btn btn-default glyphicon glyphicon-pencil\"></a>&nbsp;<a href=\"#\" class=\"btn btn-danger glyphicon glyphicon-trash\"></a></td>\n";
+								echo "	<td style=\"text-align: center; vertical-align: middle;\"><a href=\"#\" class=\"btn btn-default glyphicon glyphicon-pencil\"></a>&nbsp;<a href=\"#\" class=\"btn btn-danger glyphicon glyphicon-trash\"></a></td>\n";
 								echo "</tr>\n";
 							}
 						}
