c80c6dfa9ca4f4bceb6dd33bc365beb76b1af6f1
alternat0r <alternator99@gmail.com>
2016-05-13 22:17:06 +0800

    add jquery check & validation to every answer and response back to client.

--- play.php
+++ play.php
@@ -33,18 +33,18 @@
 						echo "	<td style=\"vertical-align: middle;\">" . $user_count . "</td>\n";
 						echo "	<td width=\"70%\" style=\"vertical-align: middle;\">" . $row['question'] . "</td>\n";
 
-						echo "	<td style=\"vertical-align: middle;\"><div style=\"color:#E6E6E6\" id=\"demo\"><div class=\"glyphicon glyphicon-question-sign\"  style=\"color:#E6E6E6\" ></div></div></td>";
+						echo "	<td style=\"vertical-align: middle;\"><div style=\"color:#E6E6E6\" id=\"a".$user_count."\"><div class=\"glyphicon glyphicon-question-sign\"  style=\"color:#E6E6E6\" ></div></div></td>\n";
 
-						echo "	<td style=\"text-align: center; vertical-align: middle; \">";
-						echo "		<div class=\"input-group\">";
-						echo "			<input class=\"form-control\" type=\"text\" id=\"userInput\" placeholder=\"Answer here\" />";
-						echo "			<span class=\"input-group-btn\">";
-						echo "				<button type=\"button\" id=\"checkAnswer\" class=\"btn btn-default \" title=\"Click to check your answer\">";
-						echo "					<span class=\"glyphicon glyphicon-circle-arrow-right\"></span>&nbsp;";
-						echo "				</button>";
-						echo "			</span>";
-						echo "		</div>";
-						echo "	</td>";
+						echo "	<td style=\"text-align: center; vertical-align: middle; \">\n";
+						echo "		<div class=\"input-group\">\n";
+						echo "			<input class=\"form-control\" type=\"text\" id=\"userInput".$user_count."\" placeholder=\"Answer here\" />\n";
+						echo "			<span class=\"input-group-btn\">\n";
+						echo "				<button".$user_count." type=\"button\" class=\"checkAnswer btn btn-default \" title=\"Click to check your answer\">\n";
+						echo "					<span class=\"glyphicon glyphicon-circle-arrow-right\"></span>&nbsp;\n";
+						echo "				</button".$user_count.">\n";
+						echo "			</span>\n";
+						echo "		</div>\n";
+						echo "	</td>\n";
 						echo "</tr>\n";
 					}
               ?>
@@ -55,13 +55,21 @@
 
 <script>
 $(document).ready(function() {    
-    $("button").click(function() {
-        var userInput = document.getElementById("userInput").value;
-        $.post("valid.php", { answer: userInput },
-        function( data, status ) {
-           	document.getElementById("demo").innerHTML = data;
-       
-        });
-    });
+	<?php
+		for ($i=1; $i<$user_count+1; $i++) {
+			echo "	butt(\"button".$i."\", \"userInput".$i."\", \"a".$i."\");\n";
+		}
+	?>
 });
+function butt( button, userInput, outputId ) {
+    $(button).click(function() {
+        var userInput1 = document.getElementById(userInput).value; // unserInput = to take user input
+        $.post("valid.php", { answer: userInput1 },
+        function( data, status ) {
+           	document.getElementById(outputId).innerHTML = data; //a2 = place to display       
+        });
+
+    });
+}
+
 </script>
