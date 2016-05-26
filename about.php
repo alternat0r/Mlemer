52823bab9f1117352092c02a09a0c5d2f9ef6ceb
Kamil Alta <alternator99@gmail.com>
2016-05-26 23:45:48 +0800

    Update about.php

--- about.php
+++ about.php
@@ -4,12 +4,28 @@
 	}
 	
 	if ( empty( $pg_about ) ) {
-		$about = "<div class=\"bs-callout bs-callout-info hoverDiv\" id=\"callout-input-needs-type\">\n";
+		$about = "<div class=\"row\">";
+  		$about .= "<div class=\"col-md-6\">";
+		$about .= "<div class=\"bs-callout bs-callout-info hoverDiv\" id=\"callout-input-needs-type\">\n";
 		$about .= "	<h4>About</h4>";
 		$about .= "	<p>";
 		$about .= "		Mlemer is a quiz, exercise, CTF or questionnaire system designed for trainer. It is designed meant to be simplified and easy to manage. This system is not suitable to be used for public access. It is designed for local network only and to assist trainer. Used at your own risk.";
 		$about .= "	</p>\n";
 		$about .= "</div>\n";
+		$about .= "</div>";
+		$about .= "<div class=\"col-md-6\">";
+		$about .= "<div class=\"bs-callout bs-callout-info hoverDiv\" id=\"callout-input-needs-type\">\n";
+		$about .= "	<h4>Credit</h4>";
+		$about .= "	<p>";
+		$about .= " 	<ul>";
+		$about .= "		<li>Twitter Bootstrap v3.3.6</li>";
+	     	$about .= "		<li>Dashboard Statistic inspired by SB Admin v2.0.</li>";
+	     	$about .= "		<li>Jquery</li>";
+	     	$about .= " 	<ul>";
+		$about .= "	</p>\n";
+		$about .= "</div>\n";
+		$about .= "</div>";
+		$about .= "</div>";
 	} else {
 		$about = $pg_about;
 	}
