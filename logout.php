aa992fd201a54d7f6bb476a49a69ac7fc7dc22f9
Kamil Alta <alternator99@gmail.com>
2016-05-27 00:04:19 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,11 @@
- <?php
-
+<?php
+	require_once "inc/config.php";
+	
+	if ( ENABLE_ERROR_MSG == "true" ) {
+		error_reporting( E_ALL );
+	} else {
+	    error_reporting( 0 );
+	}
  	if(isset($_SESSION['login_user'])) unset($_SESSION['login_user']);
  	header('Refresh: 1; URL = ?p=login');
 
