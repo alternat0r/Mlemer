d7ee008e9d966ef1ed7fc9021aea78a9d6be5fc4
Kamil Alta <alternator99@gmail.com>
2016-05-26 23:35:30 +0800

    Update about.php

--- about.php
+++ about.php
@@ -4,7 +4,14 @@
 	}
 	
 	if ( empty( $pg_about ) ) {
-		$about = "Mlemer is a quiz, exercise, CTF or questionnaire system designed for trainer. It is designed meant to be simplified and easy to manage. This system is not suitable to be used for public access. It is designed for local network only and to assist trainer. Used at your own risk.";
+		$about = "<div class=\"bs-callout bs-callout-info hoverDiv\" id=\"callout-input-needs-type\">\n";
+		$about .= "<a class=\"NoUnderLine\" href=\"javascript:void(0)\" onclick=\"window.location.href='?p=play&e=".$row['id']."'\">";
+		$about .= "	<h4>About</h4>";
+		$about .= "	<p>";
+		$about .= "		Mlemer is a quiz, exercise, CTF or questionnaire system designed for trainer. It is designed meant to be simplified and easy to manage. This system is not suitable to be used for public access. It is designed for local network only and to assist trainer. Used at your own risk.";
+		$about .= "	</p>\n";
+		$about .= "</a>";
+		$about .= "</div>\n";
 	} else {
 		$about = $pg_about;
 	}
